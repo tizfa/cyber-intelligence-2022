d4a25256d019cb3c0f69ac1f2bb5e163d2afc945
Tiziano Fagni <tiziano.fagni@iit.cnr.it>
2022-06-10 21:35:28 +0200

    Updated examples.

--- cint/esercitazione/elastic/earthquakes/import_earthquakes.py
+++ cint/esercitazione/elastic/earthquakes/import_earthquakes.py
@@ -1,11 +1,16 @@
 import pandas as pd
-import sys 
+import sys
+import json
 
 df = pd.read_csv(sys.argv[1])
 df["Location"] = df.apply(lambda r: str(r["Latitude"])+","+str(r["Longitude"]), axis=1)
-for index, r in df.iterrows():
-    jsonRow = r.to_json()
+df["Timestamp"] = df['Date'] + ' ' + df['Time']
+df = df.fillna(value=df.mean())
+jsonRowList = df.to_dict(orient='records')
+for r in jsonRowList:
+    jsonRow = json.dumps(r)
     print(jsonRow)
+
 
 
 
